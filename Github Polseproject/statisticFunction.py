5ac14bbdef955adf8fadd1674d5fb45db9ae6310
Johnathandtu <chraajensen@gmail.com>
2017-02-07 14:59:59 +0100

    Added statesticsFunction

---
+++ Github Polseproject/statisticFunction.py
@@ -0,0 +1,28 @@
+from numpy import size, sum, array
+from math import sqrt
+
+class analyzeData:
+    def __init__(self,dArray):
+        self.__setMean(dArray)
+        self.__setVariance(dArray)
+
+
+    def __setMean(self,dArray):
+        self.__meanVal = sum(dArray)/size(dArray)
+    def mean(self):
+        """Returns the equivalent mean to the array
+            dArray       [array] data of type array"""
+        return self.__meanVal
+
+
+    def __setVariance(self,dArray):
+        self.__varianceVal = sqrt(sum((dArray-self.mean())**2)/(size(dArray)-1))
+    def var(self):
+        """Variance"""
+        return self.__varianceVal
+
+
+
+if __name__ == "__main__":
+    arr = array([[1,2,3],[2,3,4],[4,5,6],[6,7,8]])
+    test = analyzeData(arr)
