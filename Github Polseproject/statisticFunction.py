eb7c8dc3107612c3ee09ec2123f2eabd2f3d7be7
Johnathandtu <chraajensen@gmail.com>
2017-02-08 06:13:04 +0100

    Opdatering af statestic funktino

--- Github Polseproject/statisticFunction.py
+++ Github Polseproject/statisticFunction.py
@@ -1,8 +1,8 @@
-from numpy import size, sum, array
+from numpy import size, sum, array, shape
 from math import sqrt
 from multiSpecPic import multiSpecPic
 
-class analyzeData:
+class analyzeLayer:
     def __init__(self,dArray):
         self.__setMean(dArray)
         self.__setVariance(dArray)
@@ -11,22 +11,27 @@ class analyzeData:
         self.__meanVal = sum(dArray)/size(dArray)
     def mean(self):
         """Returns the equivalent mean to the array
-            dArray       [array] data of type array"""
+                dArray       [array] data of type array"""
         return self.__meanVal
 
     def __setVariance(self,dArray):
         self.__varianceVal = sqrt(sum((dArray-self.mean())**2)/(size(dArray)-1))
     def var(self):
-        """Variance"""
+        """Returns the equivalent variance to the array
+                dArray       [array] data of type array"""
         return self.__varianceVal
 
+'''
+    def __setSigma(self,dArray):
+        """Returns the covariance to the array
+                dArray       [array] data of type array"""
+            return sum((dArray))
+'''
 
-#    def __setSigma(self,dArray)
-
+#Main
 if __name__ == "__main__":
     pic = multiSpecPic("01")
-    print(pic.data)
-
-#    arr = array([[1,2,3],[2,3,4],[4,5,6],[6,7,8]])
-#    test = analyzeData(arr)
-#    print(test.var())
+    data = pic.data[:,:,0]
+    print(shape(data))
+    test = analyzeLayer(data)
+    print(test.var(),test.mean())
