c745466b75cb1dea044923361e7f560b6b166616
Sebastian Meier <sebastiandmeier@gmail.com>
2017-02-14 14:27:12 +0100

    Finished mean function

--- Github Polseproject/functions.py
+++ Github Polseproject/functions.py
@@ -7,35 +7,39 @@
 # Import #
 ##########
 from multiSpecPic import data
+import numpy as np
 
-
-def annoMean(pic1, pic2):
+def annoMean(spec, anno, annoColor=0):
     """
     Funktionen finder middelværdier for pixels i
 
     input
     --------------
-    pic1 [Array]:
+    spec [Array]:
         multispectral picture as array of pixels
-    pic2 [Array]:
+    anno [Array]:
         annotation picture as array of pixels
-
+    annoColor [int]:
+        0 for blue
+        1 for green
     output
     ---------------
-    array of mean values one float per spectre
+    tuple of blue and green arrays of mean values one float per spectre
     """
 
-    print(pic1)
-    print(pic2)
+    meanArray = []
 
-    """
-    middenværdi = []
-    for i to m:
-        g = Data[Anno = (0,0,255))]
-        mu = mean(g[:,i])
+    if annoColor:
+            mask = spec[:,:,i][anno == (0,255,0)]
+    else:
+            mask = spec[:,:,i][anno == (0,0,255)]
 
-    g [[1..19],[1..19]..m]
-    """
+    for i in np.arange(len(spec[0,0,:])):
+        valueArray = spec[mask]
+        meanArray.append(mean(blueValueArray))
+
+    return meanArray
+
 
 if __name__ == "__main__":
     annoMean(data.spec(),data.anno())
