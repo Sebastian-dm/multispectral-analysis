882b6a5029c6051f6040851d3b93b479f63df4e8
Johnathandtu <chraajensen@gmail.com>
2017-02-14 14:25:55 +0100

    dataload opdate

--- Github Polseproject/multiSpecPic.py
+++ Github Polseproject/multiSpecPic.py
@@ -4,33 +4,42 @@ Comment til branch 01
 """
 from numpy import shape, array
 from scipy.io import loadmat
+#from scipy.misc import imread
 from PIL.Image import fromarray, open
 from os.path import dirname
 
 class data:
-    def __init__(self, day="01"):
+    def __init__(self, day="01"):#, save_img='spec'
         self.day = day
+        '''
+        if save_img == 'anno':
+            self.img = self.anno()
+        elif save_img == 'png':
+            self.img = self.png()
+        elif save_img == 'spec':
+            self.img = self.spec()
+        '''
+    def anno(self):
+        path = dirname(__file__) + "/data/annotation_day%s.png" % self.day
+        return array(open(path))
+
+    def png(self):
+        path = dirname(__file__) + "/data/color_day%s.png" % self.day
+        return array(open(path))
 
     def spec(self):
-        spec_path = dirname(__file__) + "/data/multispectral_day%s.mat" % self.day
-        return loadmat(spec_path)["immulti"]
+        path = dirname(__file__) + "/data/multispectral_day%s.mat" % self.day
+        return loadmat(path)["immulti"]
 
+    '''
     def save(self):
-        for i in range(shape(self.spec())[2]):
-            savepath = dirname(__file__) + "/output/lag%s.jpg" % i
-            img = fromarray(self.spec()[:,:,i])
-            img.save(savepath)
-
-    def anno(self):
-        anno_path = dirname(__file__) + "/data/annotation_day%s.png" % self.day
-        return array(open(anno_path).convert('L'))
-
-#    def png:
-
+        for i in range(shape(self.img)[2]):
+            path = dirname(__file__) + "/output/lag%s.jpg" % i
+            img = fromarray(self.img[:,:,i])
+            img.save(path)
+    '''
 
 # MAIN #
 if __name__ == "__main__":
     dataload = data("01")
-    print((dataload.anno()))
-    #.spec()[:,:,0])
-    dataload.save()
+    print(shape(dataload.spec()))
