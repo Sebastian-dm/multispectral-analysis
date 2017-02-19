8e1bd428207975a0daa196c17e5c0705f122d4d4
Johnathandtu <chraajensen@gmail.com>
2017-02-19 11:54:57 +0100

    dataLoad

--- Github Polseproject/dataLoad.py
+++ Github Polseproject/dataLoad.py
@@ -2,7 +2,7 @@
 """
 Comment til branch 01
 """
-from numpy import shape, array
+from numpy import shape, array, empty_like, ma, all, array_equal, equal, reshape, arange, zeros_like
 from scipy.io import loadmat
 #from scipy.misc import imread
 from PIL.Image import fromarray, open
@@ -45,25 +45,59 @@ class data:
         """
         return self.__getSpec
 
+    ### MASKED SPEKTRALBÅND ###
+    def specMasked(self):
+        #Create array of black values [0,0,0], False values [0,0,..,nLayers]
+        annoArr = self.anno()
+        specArr = self.spec()
+        blackArr = zeros_like(annoArr)
+        nLayers = len(specArr[0,0,:])
+        maskArr = empty_like(specArr)
+
+        maskCount = 0
+        for i in arange(shape(annoArr)[0]):
+            for j in arange(shape(annoArr)[1]):
+                if all(annoArr[i,j] == [0,0,0]):
+                    maskCount += 1
+                    maskArr[i,j] = array([True] * nLayers)
+
+        maskedArr = ma.masked_where(maskArr, specArr)
+        print("Masked percentage: ",int(maskCount/(514*514)*100))
+        return maskedArr
+
+    ### SAVE ###
+    def save(self, data="spec"):
+
+        if data == 'anno':
+            path = dirname(__file__) + "/output/png.jpg"
+            img = fromarray(self.anno())
+            img.save(path)
+
+        elif data == 'png':
+            path = dirname(__file__) + "/output/png.jpg"
+            img = fromarray(self.png())
+            img.save(path)
+
+        elif data == 'spec':
+            for i in range(shape(self.spec())[2]):
+                path = dirname(__file__) + "/output/lag%s.jpg" % i
+                img = fromarray(self.spec()[:,:,i])
+                img.save(path)
+
+        elif data == 'specMasked':
+            #For hvert lag, lav seperat filnavn og gem billede
+            dataArr = self.specMasked()
+            for i in range(shape(dataArr)[2]):
+                path = dirname(__file__) + "/output/lag%s.jpg" % i
+                img = fromarray(dataArr[:,:,i])
+                img.save(path)
+
 # MAIN #
 if __name__ == "__main__":
-    dataload = data("01")
-    print(shape(dataload.spec()))
-    print((dataload.spec()))
+    data = data("01")
+    print(shape(data.spec()))
+    print((data.spec()))
 
-    
-    '''
-    if save_img == 'anno':
-        self.img = self.anno()
-    elif save_img == 'png':
-        self.img = self.png()
-    elif save_img == 'spec':
-        self.img = self.spec()
-    '''
-    '''
-    def save(self):
-        for i in range(shape(self.img)[2]):
-            path = dirname(__file__) + "/output/lag%s.jpg" % i
-            img = fromarray(self.img[:,:,i])
-            img.save(path)
-    '''
+    #print(shape(data01.spec()))
+    #print((data01.anno()))
+    data.save("specMasked")
